8588471b21cf1e28f1bbbd3e60304adbfb138c86
mung0722 <mounyho0722@naver.com>
2024-12-01 05:55:07 +0900

    완성

--- script/hover.js
+++ script/hover.js
@@ -1,12 +1,12 @@
-// 모든 'hover' 클래스 이미지를 선택
-const hoverImages = document.querySelectorAll('.hover');
+const images = document.querySelectorAll('.hover');
 
-hoverImages.forEach((img) => {
-    img.addEventListener('mouseover', () => {
-        img.style.display = 'none'; // 마우스를 올리면 이미지 숨기기
+images.forEach((img) => {
+    const parent = img.parentElement;
+    parent.addEventListener('mouseover', () => {
+        img.classList.add('hidden');
     });
 
-    img.addEventListener('mouseout', () => {
-        img.style.display = 'block'; // 마우스를 떼면 이미지 다시 보이기
+    parent.addEventListener('mouseout', () => {
+        img.classList.remove('hidden');
     });
 });
---
+++ script/signup.js
@@ -0,0 +1,6 @@
+const button = document.querySelector('.button');
+
+button.addEventListener('click', function() {
+    alert('회원가입이 완료되었습니다!');
+    window.location.href = '../index.html';
+});
